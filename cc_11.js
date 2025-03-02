9c9c8e5865c1e80003aa09cd4fa3728315e9d09a
Ramy <ramy3@usf.edu>
2025-03-02 17:19:50 -0500

    "Task 1 - Created Book Class"

--- cc_11.js
+++ cc_11.js
@@ -1,22 +1,22 @@
 // Task 1: Creating a Book Class
-class Book {
+class Book {                           // create a class named book 
     constructor(title, author, isbn, copies) {
-        this.title = title;
+        this.title = title;              // make every single part of the constructor apart and equal to what it is 
         this.author = author;
         this.isbn = isbn;
         this.copies = copies;
     }
     getDetails() {
-        return `Title: ${this.title},
+        return `Title: ${this.title},         // we want the things to retuen in the console log to show what it is 
          Author: ${this.author}, 
          ISBN: ${this.isbn}, 
          Copies: ${this.copies}`;
-    }
+    } // everytime we need to pull it up in the console it will show what it has to show 
 
     updateCopies(amount) {
         this.copies += amount;
     }
-}
+};
 
 const book1 = new Book("The Great Gatsby", "F. Scott Fitzgerald", 123456, 5);
 console.log(book1.getDetails());
@@ -24,17 +24,17 @@ book1.updateCopies(-1);
 console.log(book1.getDetails());
 
 // Task 2: Creating a Borrower Class
-class Borrower {
-    constructor(name, borrowerId) {
-        this.name = name;
-        this.borrowerId = borrowerId;
+class Borrower {                 // creating a class called borrower 
+    constructor(name, borrowerId) {      // 
+        this.name = name;         // assign it a name 
+        this.borrowerId = borrowerId;       // assign it to a borrower id 
         this.borrowedBooks = [];
     }
-    borrowBook(bookTitle) {
-        this.borrowedBooks.push(bookTitle);
+    borrowBook(bookTitle) {            // method out the book title to the borrow book idea 
+        this.borrowedBooks.push(bookTitle);    // add a title to the book that was borrowed 
     }
-    returnBook(bookTitle) {
-        this.borrowedBooks = this.borrowedBooks.filter(title => title !== bookTitle);
+    returnBook(bookTitle) {            // how to return the book from borrowing 
+        this.borrowedBooks = this.borrowedBooks.filter(title => title !== bookTitle);           // remove the book title 
     }
 }
 
@@ -47,10 +47,10 @@ console.log(borrower1.borrowedBooks);
 // Task 3: Creating a Library Class
 class Library {
     constructor() {
-        this.books = [];
-        this.borrowers = [];
+        this.books = [];        //start an empty list of books  
+        this.borrowers = []; //haeve a list of empty borrowers 
     }
-    addBook(book) {
+    addBook(book) {      // add book to library
         this.books.push(book);
     }
     listBooks() {
@@ -63,14 +63,14 @@ library.addBook(book1);
 library.listBooks();
 
 // Task 4: Implementing Book Borrowing
-library.borrowers.push(borrower1);
-library.lendBook = function (borrowerId, isbn) {
+library.borrowers.push(borrower1);     // adding borrower to library system 
+library.lendBook = function (borrowerId, isbn) {       // lend a book function
     let book = this.books.find(b => b.isbn === isbn);
     let borrower = this.borrowers.find(b => b.borrowerId === borrowerId);
 
-    if (book && book.copies > 0 && borrower) {
+    if (book && book.copies > 0 && borrower) {       // see if book exists and if there is copies 
         book.updateCopies(-1);
-        borrower.borrowBook(book.title);
+        borrower.borrowBook(book.title);      // add title to borrowed books 
     }
 };
 
@@ -79,8 +79,8 @@ console.log(book1.getDetails());
 console.log(borrower1.borrowedBooks);
 
 // Task 5: Implementing Book Returns
-library.returnBook = function (borrowerId, isbn) {
-    let book = this.books.find(b => b.isbn === isbn);
+library.returnBook = function (borrowerId, isbn) {       // find book by unique id 
+    let book = this.books.find(b => b.isbn === isbn);         // find bool by book nsme 
     let borrower = this.borrowers.find(b => b.borrowerId === borrowerId);
 
     if (book && borrower && borrower.borrowedBooks.includes(book.title)) {
